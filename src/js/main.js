2dca0e98482d2b8008f9ef574c15ea780fca4cd7
angelique-df <faye.angelique.d@gmail.com>
2022-07-20 17:30:02 +0000

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import MobileMenu from "./MobileMenu";
 
 const mobileMenuToggler = MobileMenu("header");
 
+// If the user resizes the window when the mobile menu is open, make sure the menu is not visible if the screen width is above 1440px.
 window.addEventListener("resize", function () {
 	mobileMenuToggler.removeMobileMenu();
 });
